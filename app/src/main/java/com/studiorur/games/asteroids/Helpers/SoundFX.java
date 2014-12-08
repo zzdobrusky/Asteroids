af2fe7f3be2455eb53450eb20451182636ad5054
zzdobrusky <zzdobrusky@gmail.com>
2014-12-08 13:26:04 -0700

    trying to fix sound - take 1

--- app/src/main/java/com/studiorur/games/asteroids/Helpers/SoundFX.java
+++ app/src/main/java/com/studiorur/games/asteroids/Helpers/SoundFX.java
@@ -13,6 +13,7 @@ public class SoundFX
 {
     private SoundPool _soundPool;
     private HashMap<Integer, Integer> _soundIds;
+    private HashMap<Integer, Boolean> _soundsLoaded;
     private static SoundFX _instance = null;
 
     public static SoundFX getInstance()
@@ -34,30 +35,34 @@ public class SoundFX
     {
         _soundPool = new SoundPool(10, AudioManager.STREAM_MUSIC, 0);
         _soundIds = new HashMap<Integer, Integer>();
+        _soundsLoaded = new HashMap<Integer, Boolean>();
     }
 
     public void addSound(Context context, final int soundIdentifier)
     {
         // load the sound
         _soundPool.load(context, soundIdentifier, 1);
+        _soundsLoaded.put(soundIdentifier, false);
         _soundPool.setOnLoadCompleteListener(new SoundPool.OnLoadCompleteListener()
         {
             @Override
             public void onLoadComplete(SoundPool soundPool, int soundId, int status)
             {
                 _soundIds.put(soundIdentifier, soundId);
+                _soundsLoaded.put(soundIdentifier, true);
             }
         });
     }
 
     public void play(int soundIdentifier, float volume)
     {
-        if(_soundIds.get(soundIdentifier) > 0 && _soundPool != null)
+        if(_soundIds.get(soundIdentifier) > 0 && _soundPool != null && _soundsLoaded.get(soundIdentifier) == true)
             _soundPool.play(_soundIds.get(soundIdentifier), volume, volume, 1, 0, 1f);
     }
 
-    public void stop()
+    public void stop(int soundIdentifier)
     {
         // not used
+
     }
 }
--- app/src/main/java/com/studiorur/games/asteroids/Shapes/Projectile.java
+++ app/src/main/java/com/studiorur/games/asteroids/Shapes/Projectile.java
@@ -2,12 +2,15 @@ package com.studiorur.games.asteroids.Shapes;
 
 import android.graphics.PointF;
 
+import com.studiorur.games.asteroids.Helpers.Boundary;
+import com.studiorur.games.asteroids.Interfaces.CollidableType;
+import com.studiorur.games.asteroids.Interfaces.ICollidable;
 import com.studiorur.games.asteroids.Interfaces.IUpdatable;
 
 /**
  * Created by zbynek on 12/8/2014.
  */
-public class Projectile extends RectangleShape implements IUpdatable
+public class Projectile extends RectangleShape implements IUpdatable, ICollidable
 {
     PointF _velocity = new PointF(0.0f, 0.0f);;
 
@@ -33,5 +36,23 @@ public class Projectile extends RectangleShape implements IUpdatable
         // physics here
         _center.x = _center.x + _velocity.x * time;
         _center.y = _center.y + _velocity.y * time;
+    }
+
+    @Override
+    public Boundary getBoundery()
+    {
+        return null;
+    }
+
+    @Override
+    public CollidableType getCollidableType()
+    {
+        return null;
+    }
+
+    @Override
+    public boolean isColliding(ICollidable object)
+    {
+        return false;
     }
 }
